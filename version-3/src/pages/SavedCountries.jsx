130936d4da30cd6d54e701afabf4e7782436b97d
broscoe <bakari.roscoe@gmail.com>
2025-01-29 13:35:38 -0500

    added localStorage and dataExercise1

---
+++ version-3/src/pages/SavedCountries.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+
+export default function savedCountries() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    country: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let dataObj = formData;
+    console.log(dataObj);
+    setFormData({ name: '', email: '', country: '' }); // Reset the form
+    //send dataObj to backend
+      localStorage.setItem("profile", JSON.stringify(dataObj)
+    // const db = getDatabase();
+    // set(ref(db, 'users/' + userId), {
+    //   username: dataObj.name,
+    //   email: dataObj.email,
+    //   country: dataObj.imageUrl,
+    //   bio
+    // });
+  };
+
+  return (
+    <>
+
+
+      <form onSubmit = {handleSubmit}>
+        <label htmlFor = "name"> </label>
+        <input
+          placeholder = "Name"
+          type = "text"
+          name = "name"
+          id = "name"
+          value = {formData.name}
+          onChange = {handleChange}
+        />
+        <label htmlFor = "email"></label>
+        <input
+          placeholder = "Email"
+          type = "email"
+          name = "email"
+          id = "email"
+          value = {formData.email}
+          onChange = {handleChange}
+        />
+        <label htmlFor = "country"></label>
+        <input
+          placeholder = "Country"
+          type= "country"
+          name =" country"
+          id = "country"
+          value = {formData.country}
+          onChange = {handleChange}
+        />
+        <label htmlFor="bio">
+
+          <textarea value = {formData.bio} onChange= {handleChange} placeholder="Bio" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  )
+}
